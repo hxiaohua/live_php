8437c810d8281e4f3efc2f33a56bc9bd98de1845
hxiaohua <811836659@qq.com>
2022-03-21 16:20:39 +0800

    Update bottom.php

--- teamplate/bottom.php
+++ teamplate/bottom.php
@@ -2,7 +2,7 @@
   <h3> <a href="../course/wx" class="btn btn-primary">课程中心</a>
 	  <a href="../live" class="btn btn-primary" target="_blank">直播中心</a>
 	  <a href="../" class="btn btn-primary">网站首页</a> </h3>
-  <div class="alert alert-info" role="alert"> Moodle课程中心，开发者微信：cutehxh </div>
+  <div class="alert alert-info" role="alert"> 直播相关，如需付费咨询请联系开发者微信：cutehxh </div>
 </div>
 </div>
 </body>
